3ba6fbe64f4120fc94552e5357a51e86e72da26b
Alisa Parashchenko <parashchenko.alisa@yandex.ru>
2020-01-09 20:16:09 +0000

    Bessere gradient()-Methode
    gradient() braucht jetzt nicht mehr den Startwert als Argument, und es funktionieren jetzt auch lange Zeiten und dunklere Farben, und der Code ist etwas übersichtlicher geworden.

--- bandfkt.py
+++ bandfkt.py
@@ -4,6 +4,16 @@ import neopixel
 
 pixels = 0
 
+class GradientPixel:
+	def __init__(self, num, colour, target, ltime):
+		self.num = num
+		self.colour = colour
+		self.step = getSteps(colour, target, ltime)
+	def doStep(self):
+		self.colour = (self.colour[0] + self.step[0], self.colour[1] + self.step[1], self.colour[2] + self.step[2])
+	def getColour(self):
+		return (pas(self.colour[0]), pas(self.colour[1]), pas(self.colour[2]))
+
 def initNeopixels(num, order):
 	global pixels
 	pixel_pin = board.D18
@@ -11,6 +21,11 @@ def initNeopixels(num, order):
 	ORDER = order
 	pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
 	return pixels
+
+def getSteps(start, end, ltime): # start, end: Tupel mit 3 Werten; ltime: Zeit in Sekunden
+	colDiff = (end[0] - start[0], end[1] - start[1], end[2] - start[2])
+	step_time = 95 # Bruchteil einer Sekunde, um einen Farb-Schritt zu machen; durch Ausprobieren bestimmt
+	return (colDiff[0] / ltime / step_time, colDiff[1] / ltime / step_time, colDiff[2] / ltime / step_time)
 
 def pas(num): # Funktion, um einen Wert zu geben, der sicher für den Leuchtstreifen passt
 	if num < 0:
@@ -20,24 +35,18 @@ def pas(num): # Funktion, um einen Wert zu geben, der sicher für den Leuchtstre
 	else:
 		return int(num)
 
-def getSteps(start, end, ltime): # start, end: Tupel mit 3 Werten; ltime: Zeit in Sekunden
-	colDiff = (end[0] - start[0], end[1] - start[1], end[2] - start[2])
-	step_time = 95 # Bruchteil einer Sekunde, um einen Farb-Schritt zu machen; durch Ausprobieren bestimmt
-	return (colDiff[0] / ltime / step_time, colDiff[1] / ltime / step_time, colDiff[2] / ltime / step_time)
 
-def gradient(which_pixels, start, stop, ltime):
+def gradient(which_pixels, stop, ltime):
 	global pixels
+	gradientPixels = []
 	for j in which_pixels:
-		pixels[j] = start
+		gradientPixels.append(GradientPixel(j, pixels[j], stop, ltime))
 	beginTime = time.time()
-	steps = getSteps(start, stop, ltime)
-##	i = 0
 	while time.time() - beginTime <= ltime:
-##		print(str(i) + " " + str((pixels[0][0], pixels[0][1], pixels[0][2])))
-		for j in which_pixels:
-			pixels[j] = (pas(pixels[j][0] + steps[0]), pas(pixels[j][1] + steps[1]), pas(pixels[j][2] + steps[2]))
+		for pix in gradientPixels:
+			pix.doStep()
+			pixels[pix.num] = pix.getColour()
 		pixels.show()
-##		i += 1
 		time.sleep(0.01)
 
 def flackern(which_pixels):
